0d08bb0e2411afe9b1d99a471d77a34189c86b16
defauth98 <neto.daniribeiro@gmail.com>
2021-05-22 11:57:57 -0300

    feat: layout do app

--- src/components/MovieCard/index.jsx
+++ src/components/MovieCard/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from './Rating';
+import Rating from '../Rating';
+
+import './styles.css'; 
 
 function MovieCard({ movie }) {
   const { title, subtitle, storyline, imagePath, rating } = movie;
 
   return (
-    <div>
+    <div className="movie-card">
+      <img src={ imagePath } alt={ title } />
       <h4>{ title }</h4>
       <h5>{ subtitle }</h5>
       <p>{ storyline }</p>
-      <img src={ imagePath } alt={ title } />
       <Rating rating={ rating } />
     </div>
   );
--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import MovieCard from './MovieCard';
+import MovieCard from '../MovieCard';
+
+import './styles.css'; 
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
-      <div>
+      <div className="movie-list">
         { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
       </div>
     );
