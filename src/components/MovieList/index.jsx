9179a934e0c30874d7506a9203c9ab23d14678f1
defauth98 <neto.daniribeiro@gmail.com>
2021-07-14 20:02:21 -0300

    feat: colocando borda arrodondadas nos cards e estilizando o header

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MovieCard from '../MovieCard';
 
-import './styles.css'; 
+import './styles.css';
 
 class MovieList extends React.Component {
   render() {
     const { movies } = this.props;
     return (
       <div className="movie-list">
-        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        <div className="cards">
+          { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        </div>
       </div>
     );
   }
